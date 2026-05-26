3e8c7cecc6803adb716a84d64cdf58d8c921189f
Pravesh-Shrestha <sthapravesh12@gmail.com>
2026-05-26 10:10:00 +0545

    feat: implement user registration with bcrypt password hashing

---
+++ pawstore-backend/src/routes/userRoutes.ts
@@ -0,0 +1,4 @@
+import express from "express"; const router = express.Router();
+import { registerUser } from "../controllers/userController";
+router.post("/", registerUser);
+export default router;
